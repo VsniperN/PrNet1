cc12d5fb6ae68adc9d6d23fcebf49eb6782f6c7a
VsniperN <ruslan.abdukarimov@nure.ua>
2024-12-30 05:01:30 +0500

    net1
    net1

---
+++ AVGSUMVECT/ManageVector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVGSUMVECT
+{
+    public class ManageVector
+    {
+        private List<int> vector = new List<int>();
+
+
+        public void Add_Element_To_Vector(int value)
+        {
+            vector.Add(value);
+        }
+
+
+        public int ParallelSum(int numThreads)
+        {
+            if (vector.Count == 0) return 0;
+
+            int sum = 0;
+            int partSize = vector.Count / numThreads;
+
+            Task<int>[] tasks = new Task<int>[numThreads];
+
+
+            for (int t = 0; t < numThreads; t++)
+            {
+                int start = t * partSize;
+                int end = (t == numThreads - 1) ? vector.Count : start + partSize;
+
+
+                tasks[t] = Task.Run(() =>
+                {
+                    int localSum = 0;
+                    for (int i = start; i < end; i++)
+                    {
+                        localSum += vector[i];
+                    }
+                    return localSum;
+                });
+            }
+
+
+            Task.WaitAll(tasks);
+
+            foreach (var task in tasks)
+            {
+                sum += task.Result;
+            }
+            return sum / vector.Count;
+        }
+    }
+}
---
+++ AVGSUMVECT/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AVGSUMVECT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManageVector vector = new ManageVector();
+
+            for (int i = 0; i < 100000000; i++)
+            {
+                vector.Add_Element_To_Vector(i);
+            }
+
+
+            int numThreads = 10;
+            Console.WriteLine($"Parallel Sum with {numThreads} threads: {vector.ParallelSum(numThreads)}");
+
+
+            var watchSingle = System.Diagnostics.Stopwatch.StartNew();
+            int singleThreadSum = vector.ParallelSum(1);
+            watchSingle.Stop();
+            Console.WriteLine($"Single Thread Sum: {singleThreadSum}, Time: {watchSingle.ElapsedMilliseconds} ms");
+
+            var watchMulti = System.Diagnostics.Stopwatch.StartNew();
+            int multiThreadSum = vector.ParallelSum(numThreads);
+            watchMulti.Stop();
+            Console.WriteLine($"Multi Thread Sum: {multiThreadSum}, Time: {watchMulti.ElapsedMilliseconds} ms");
+
+            double speedup = (double)watchSingle.ElapsedMilliseconds / (double)watchMulti.ElapsedMilliseconds;
+            Console.WriteLine($"Speedup: {speedup}");
+        }
+    }
+}
